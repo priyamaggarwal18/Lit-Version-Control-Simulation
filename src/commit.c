96ad857d4339dc3860f0787b16416ab28a8f3c50
Priyam Aggarwal <122437027+priyamaggarwal18@users.noreply.github.com>
2024-09-04 20:15:51 +0530

    created the functions for commit
    like creating a new commit hash saving a commit and printing the commit history.

---
+++ src/commit.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "commit.h"
+
+// function to generate a hash value of a commit based on time
+void generate_hash(const char *commit_message, char *hash) {
+    time_t current_time = time(NULL);
+    snprintf(hash, 16, "%lx%lx", (unsigned long)current_time, (unsigned long)strlen(commit_message));
+}
+
+// save the commit hash and message to the commit history file
+void save_commit(const char *hash, const char *message) {
+    char commit_file[512];
+    snprintf(commit_file, sizeof(commit_file), "local_repo/.vcs/commit_history.txt");
+
+    FILE *file = fopen(commit_file, "a");
+    if (!file) {
+        perror("Failed to open commit history file");
+        return;
+    }
+    time_t current_time = time(NULL);
+    fprintf(file, "Commit: %s\nMessage: %s\nTime: %s\n\n", hash, message, ctime(&current_time));
+    fclose(file);
+}
+
+// function to print the commit history
+void print_commit_history() {
+    char commit_file[512];
+    snprintf(commit_file, sizeof(commit_file), "local_repo/.vcs/commit_history.txt");
+
+    FILE *file = fopen(commit_file, "r");
+    if (!file) {
+        perror("Failed to open commit history file");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+
+    fclose(file);
+}
--- src/commit.h
+++ src/commit.h
@@ -1,6 +1,13 @@
-#ifndef COMMIT // gaurd for commit
-#define COMMIT
+#ifndef COMMIT_H //gaurd for avoiding multiple inclusions
+#define COMMIT_H
 
-char* create_commit(const char*message,const char* category);  // function to create a new commit
+// function to generate a hash value which is similiar to a commit id in git
+void generate_hash(const char *commit_message, char *hash);
+
+// function that will maintain the logs of commits
+void save_commit(const char *hash, const char *message);
+
+// function to print my commit history
+void print_commit_history();
 
 #endif
